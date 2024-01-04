4109c616d6a3b89271730a1136fd35620f9ec797
Caleb Bredekamp <caleb_sb@icloud.com>
2024-01-04 09:39:03 +0200

    Moving to hyprdots

---
+++ Configs/.config/fish/config.fish
@@ -0,0 +1,8 @@
+alias l='eza -lh  --icons=auto' # long list
+alias ls='eza -1   --icons=auto' # short list
+alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
+alias ld='eza -lhD --icons=auto' # long list dirs
+alias lg='lazygit'
+alias ide='kitty --session ~/.config/kitty/ide.session --detach && exit'
+
+starship init fish | source
