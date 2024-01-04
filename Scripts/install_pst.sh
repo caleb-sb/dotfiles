753f6bd30c5af83fd2f1a3d143ed5dbf192915db
caleb-sb <caleb_sb@icloud.com>
2024-01-04 11:38:11 +0000

    pnpm

--- Scripts/install_pst.sh
+++ Scripts/install_pst.sh
@@ -5,53 +5,41 @@
 #|/ /---+--------------------------------------+/ /---|#
 
 source global_fn.sh
-if [ $? -ne 0 ] ; then
-    echo "Error: unable to source global_fn.sh, please execute from $(dirname "$(realpath "$0")")..."
-    exit 1
+if [ $? -ne 0 ]; then
+	echo "Error: unable to source global_fn.sh, please execute from $(dirname "$(realpath "$0")")..."
+	exit 1
 fi
 
+# pnpm
+curl -fsSL https://get.pnpm.io/install.sh | SHELL="$(which fish)" sh -
+# node
+pnpm env use --global lts
 
 # sddm
-if pkg_installed sddm
-    then
+if pkg_installed sddm; then
 
-    if [ ! -d /etc/sddm.conf.d ] ; then
-        sudo mkdir -p /etc/sddm.conf.d
-    fi
-
-    if [ ! -f /etc/sddm.conf.d/kde_settings.t2.bkp ] ; then
-        echo "configuring sddm..."
-        sudo tar -xzf ${CloneDir}/Source/arcs/Sddm_Corners.tar.gz -C /usr/share/sddm/themes/
-        sudo touch /etc/sddm.conf.d/kde_settings.conf
-        sudo cp /etc/sddm.conf.d/kde_settings.conf /etc/sddm.conf.d/kde_settings.t2.bkp
-        sudo cp /usr/share/sddm/themes/corners/kde_settings.conf /etc/sddm.conf.d/
-    else
-        echo "sddm is already configured..."
-    fi
-
-    if [ ! -f /usr/share/sddm/faces/${USER}.face.icon ] && [ -f ${CloneDir}/Source/misc/${USER}.face.icon ] ; then
-        sudo cp ${CloneDir}/Source/misc/${USER}.face.icon /usr/share/sddm/faces/
-        echo "avatar set for ${USER}..."
-    fi
+	if [ ! -d /usr/share/sddm/themes/sddm-astronaut-theme ]; then
+		echo "configuring sddm..."
+		sudo tar -xzf ${CloneDir}/Source/arcs/sddm-astronaut-theme.tar.gz -C /usr/share/sddm/themes/
+		echo "[Theme]
+    Current=sddm-astronaut-theme" | sudo tee /etc/sddm.conf
+	else
+		echo "sddm is already configured..."
+	fi
 
 else
-    echo "WARNING: sddm is not installed..."
+	echo "WARNING: sddm is not installed..."
 fi
-
 
 # dolphin
-if pkg_installed dolphin && pkg_installed xdg-utils
-    then
+if pkg_installed dolphin && pkg_installed xdg-utils; then
 
-    xdg-mime default org.kde.dolphin.desktop inode/directory
-    echo "setting" `xdg-mime query default "inode/directory"` "as default file explorer..."
+	xdg-mime default org.kde.dolphin.desktop inode/directory
+	echo "setting" $(xdg-mime query default "inode/directory") "as default file explorer..."
 
 else
-    echo "WARNING: dolphin is not installed..."
+	echo "WARNING: dolphin is not installed..."
 fi
-
 
 # shell
 ./restore_shl.sh ${getShell}
-
-
